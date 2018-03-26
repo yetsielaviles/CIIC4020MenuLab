35010062b0646923b26de87066a0f290d7a0094e
ICOM STUDENTS <yetsiel.aviles@upr.edu>
2018-03-26 12:47:41 -0400

    First

---
+++ src/menuClasses/MainMenu.java
@@ -0,0 +1,23 @@
+package menuClasses;
+
+import java.util.ArrayList;
+
+public class MainMenu extends Menu {
+	private static final MainMenu MM = new MainMenu(); 
+	private MainMenu() { 
+		super(); 
+		String title; 
+		ArrayList<Option> options = new ArrayList<Option>();  
+		title = "Main Menu"; 
+		options.add(new Option("Show all Lists", new ShowListsAction())); 
+		options.add(new Option("Create New List", new CreateListAction())); 
+		options.add(new Option("Operations on Lists", new OperateListAction())); 
+		options.add(Option.EXIT); 
+
+		super.InitializeMenu(title, options); 
+	}
+	
+	public static MainMenu getMainMenu() { 
+		return MM; 
+	}
+}
